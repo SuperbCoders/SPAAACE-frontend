5c7a9a9fb534be788b1b8dddd080bf832668ad5f
Fedor Sharaputo <sharaputo@icloud.com>
2022-08-18 19:17:58 +0300

    build folder fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,152 +1,153 @@
 // VARIABLES & PATHS
-let preprocessor = 'sass', // Preprocessor (sass, scss)
-    fileswatch = 'html, htm, txt, json, md, woff2', // List of files extensions for watching & hard reload (comma separated)
-    imageswatch = 'jpg, jpeg, png, webp, svg', // List of images extensions for watching & compression (comma separated)
-    baseDir = 'src' // Base directory path without «/» at the end
-    buildDir = 'build' // Build directory path without «/» at the end
+let preprocessor = "sass", // Preprocessor (sass, scss)
+  fileswatch = "html, htm, txt, json, md, woff2", // List of files extensions for watching & hard reload (comma separated)
+  imageswatch = "jpg, jpeg, png, webp, svg", // List of images extensions for watching & compression (comma separated)
+  baseDir = "src"; // Base directory path without «/» at the end
+buildDir = "dist"; // Build directory path without «/» at the end
 
 let paths = {
-    html: {
-        src: [
-            'src/**/*.html',
-            '!src/components/**/*.html'
-        ],
-        dest: buildDir,
-    },
-	styles: {
-		src: [
-            baseDir + '/styles/**/*.scss',
-        ],
-		dest: buildDir + '/css',
-    },
-    scripts: {
-		src: baseDir + '/js/app.js',
-        dest: buildDir + '/js',
-    },
-    libs: {
-        src: [
-            'node_modules/inputmask/dist/inputmask.min.js',
-        ],
-        dest: buildDir + '/js',
-    },
-	images: {
-		src: [
-            baseDir + '!/img/sprite/**/*',
-            baseDir + '/img/**/*.{jpg,jpeg,png,svg,webp}',
-        ],
-		dest: buildDir + '/img',
-    },
-    sprites: {
-		src: baseDir + '/img/sprite/*.svg',
-		dest: buildDir + '/img',
-    },
-    deploy: {
-        dest: './' + buildDir + '**/*',
-    },
-    jsOutputName: 'app.min.js',
-    libsOutputName: 'vendor.min.js'
-}
+  html: {
+    src: ["src/**/*.html", "!src/components/**/*.html"],
+    dest: buildDir,
+  },
+  styles: {
+    src: [baseDir + "/styles/**/*.scss"],
+    dest: buildDir + "/css",
+  },
+  scripts: {
+    src: baseDir + "/js/app.js",
+    dest: buildDir + "/js",
+  },
+  libs: {
+    src: ["node_modules/inputmask/dist/inputmask.min.js"],
+    dest: buildDir + "/js",
+  },
+  images: {
+    src: [
+      baseDir + "!/img/sprite/**/*",
+      baseDir + "/img/**/*.{jpg,jpeg,png,svg,webp}",
+    ],
+    dest: buildDir + "/img",
+  },
+  sprites: {
+    src: baseDir + "/img/sprite/*.svg",
+    dest: buildDir + "/img",
+  },
+  deploy: {
+    dest: "./" + buildDir + "**/*",
+  },
+  jsOutputName: "app.min.js",
+  libsOutputName: "vendor.min.js",
+};
 
 let options = {
-    "indent_with_tabs": true,
-    "max_preserve_newlines": 0,
+  indent_with_tabs: true,
+  max_preserve_newlines: 0,
 };
 
 // LOGIC
-const browserSync = require('browser-sync').create();
-const del = require('del');
-const { src, dest, parallel, series, watch } = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const babel = require('gulp-babel');
-const concat = require('gulp-concat');
-const include = require('gulp-file-include');
-const htmlbeautify = require('gulp-html-beautify');
-const imagemin = require('gulp-imagemin');
-const newer = require('gulp-newer');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass')(require('sass'));
-const sassglob = require('gulp-sass-glob');
-const svgSprite = require('gulp-svg-sprite');
-const uglify = require('gulp-uglify-es').default;
+const browserSync = require("browser-sync").create();
+const del = require("del");
+const { src, dest, parallel, series, watch } = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const babel = require("gulp-babel");
+const concat = require("gulp-concat");
+const include = require("gulp-file-include");
+const htmlbeautify = require("gulp-html-beautify");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass")(require("sass"));
+const sassglob = require("gulp-sass-glob");
+const svgSprite = require("gulp-svg-sprite");
+const uglify = require("gulp-uglify-es").default;
 
 function browsersync() {
-	browserSync.init({
-        server: { baseDir: buildDir + '/' },
-        browser: "google chrome",
-		notify: false
-	})
+  browserSync.init({
+    server: { baseDir: buildDir + "/" },
+    browser: "google chrome",
+    notify: false,
+  });
 }
 
 function html() {
-	return src(paths.html.src)
-        .pipe(include())
-        .pipe(htmlbeautify(options))
-        .pipe(dest(paths.html.dest))
-        .pipe(browserSync.stream())
+  return src(paths.html.src)
+    .pipe(include())
+    .pipe(htmlbeautify(options))
+    .pipe(dest(paths.html.dest))
+    .pipe(browserSync.stream());
 }
 
 function styles() {
-    return src(paths.styles.src)
-        .pipe(eval(`${preprocessor}glob`)())
-        .pipe(eval(preprocessor)({ outputStyle: 'compressed' }))
-        .pipe(autoprefixer({ overrideBrowserslist: ['last 8 versions'], grid: true }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(dest(paths.styles.dest))
-        .pipe(browserSync.stream())
+  return src(paths.styles.src)
+    .pipe(eval(`${preprocessor}glob`)())
+    .pipe(eval(preprocessor)({ outputStyle: "compressed" }))
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ["last 8 versions"], grid: true }),
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest(paths.styles.dest))
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
-	return src(paths.scripts.src)
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(dest(paths.scripts.dest))
-        .pipe(browserSync.stream())
+  return src(paths.scripts.src)
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      }),
+    )
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest(paths.scripts.dest))
+    .pipe(browserSync.stream());
 }
 
 function libs() {
-	return src(paths.libs.src)
-        .pipe(concat(paths.libsOutputName))
-        .pipe(uglify())
-        .pipe(dest(paths.libs.dest))
-        .pipe(browserSync.stream())
+  return src(paths.libs.src)
+    .pipe(concat(paths.libsOutputName))
+    .pipe(uglify())
+    .pipe(dest(paths.libs.dest))
+    .pipe(browserSync.stream());
 }
 
 function images() {
-	return src(paths.images.src)
-        .pipe(newer(paths.images.dest))
-        .pipe(imagemin())
-        .pipe(dest(paths.images.dest))
-        .pipe(browserSync.stream())
+  return src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(imagemin())
+    .pipe(dest(paths.images.dest))
+    .pipe(browserSync.stream());
 }
 
 function sprites() {
-    return src(paths.sprites.src)
-        .pipe(svgSprite({
-                mode: {
-                    stack: {
-                        sprite: "../sprite.svg"
-                    }
-                },
-            }
-        ))
-        .pipe(dest(paths.sprites.dest))
-        .pipe(browserSync.stream())
+  return src(paths.sprites.src)
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg",
+          },
+        },
+      }),
+    )
+    .pipe(dest(paths.sprites.dest))
+    .pipe(browserSync.stream());
 }
 
 function cleaningimages() {
-	return del('' + paths.images.dest + '/**/*', { force: true })
+  return del("" + paths.images.dest + "/**/*", { force: true });
 }
 
 function startwatch() {
-    watch(baseDir  + '/**/*.{' + fileswatch + '}').on('change', browserSync.reload);
-	watch(baseDir  + '/**/*.html', html);
-	watch(baseDir  + '/styles/**/*.scss', styles);
-    watch(baseDir + '/js/**/*.js', scripts);
-    watch(baseDir  + '/img/**/*.{' + imageswatch + '}', images);
-	watch(baseDir  + '/img/sprite/**/*.svg', sprites);
+  watch(baseDir + "/**/*.{" + fileswatch + "}").on(
+    "change",
+    browserSync.reload,
+  );
+  watch(baseDir + "/**/*.html", html);
+  watch(baseDir + "/styles/**/*.scss", styles);
+  watch(baseDir + "/js/**/*.js", scripts);
+  watch(baseDir + "/img/**/*.{" + imageswatch + "}", images);
+  watch(baseDir + "/img/sprite/**/*.svg", sprites);
 }
 exports.cleaningimages = cleaningimages;
 exports.sprites = sprites;
@@ -156,4 +157,13 @@ exports.scripts = scripts;
 exports.styles = styles;
 exports.html = html;
 exports.browsersync = browsersync;
-exports.default = parallel(html, styles, scripts, libs, images, sprites, browsersync, startwatch);
+exports.default = parallel(
+  html,
+  styles,
+  scripts,
+  libs,
+  images,
+  sprites,
+  browsersync,
+  startwatch,
+);
