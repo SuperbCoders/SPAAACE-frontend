cdff22d5830aa7f60f9f85484aa81f48ff90f0bb
Fedor Sharaputo <sharaputo@icloud.com>
2022-08-18 15:09:24 +0300

    header colors logic update

--- src/js/app.js
+++ src/js/app.js
@@ -1,206 +1,137 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // height 100vh fix for IOS
+document.addEventListener("DOMContentLoaded", function () {
+  // height 100vh fix for IOS
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  // resize
+  window.addEventListener("resize", () => {
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-    // resize
-    window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
+  // inputmask
+  Inputmask().mask(document.querySelectorAll("input"));
+
+  // textarea
+  document.querySelectorAll("textarea").forEach((el) => {
+    el.style.height = el.setAttribute(
+      "style",
+      "height: " + el.scrollHeight + "px",
+    );
+    el.classList.add("auto");
+    el.addEventListener("input", (e) => {
+      el.style.height = "auto";
+      el.style.height = el.scrollHeight + "px";
+    });
+  });
+
+  // mobile menu
+  const hamburger = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const mobileMenuClose = document.querySelector(".mobile-menu__close");
+
+  if (hamburger && mobileMenu && mobileMenuClose) {
+    hamburger.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      mobileMenu.classList.add("mobile-menu--active");
+      document.body.classList.add("scroll-disabled");
     });
 
-    // inputmask
-    Inputmask().mask(document.querySelectorAll('input'))
+    mobileMenuClose.addEventListener("click", (event) => {
+      event.preventDefault();
 
-    // textarea
-    document.querySelectorAll('textarea').forEach(el => {
-        el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
-        el.classList.add('auto');
-        el.addEventListener('input', e => {
-            el.style.height = 'auto';
-            el.style.height = (el.scrollHeight) + 'px';
-        });
+      mobileMenu.classList.remove("mobile-menu--active");
+      document.body.classList.remove("scroll-disabled");
     });
-    
-    // mobile menu
-    const hamburger = document.getElementById('menu-toggle')
-    const mobileMenu = document.getElementById('mobile-menu')
-    const mobileMenuClose = document.querySelector('.mobile-menu__close')
+  }
 
-    if (hamburger && mobileMenu && mobileMenuClose) {
-        hamburger.addEventListener('click', (event) => {
-            event.preventDefault()
+  // on hover view
+  const onHoverLink = document.querySelectorAll(".on-hover");
 
-            mobileMenu.classList.add('mobile-menu--active')
-            document.body.classList.add('scroll-disabled')
-        })
+  if (onHoverLink) {
+    onHoverLink.forEach((item, i) => {
+      item.addEventListener("mouseover", () => {
+        document
+          .querySelectorAll(".on-hover-view")
+          .forEach((child) => child.classList.remove("active"));
+        document.querySelectorAll(".on-hover-view")[i].classList.add("active");
+      });
 
-        mobileMenuClose.addEventListener('click', (event) => {
-            event.preventDefault()
+      item.addEventListener("mouseout", () => {
+        document
+          .querySelectorAll(".on-hover-view")
+          .forEach((child) => child.classList.remove("active"));
+      });
+    });
+  }
 
-            mobileMenu.classList.remove('mobile-menu--active')
-            document.body.classList.remove('scroll-disabled')
-        })
+  // header & logo change colors on intersecting black & white sections
+  const header = document.getElementById("header");
+  const logo = document.getElementById("fixed-logotype");
+  const sections = [...document.querySelectorAll("[data-section]")];
+  const scrollRoot = document.querySelector("[data-scroller]");
+
+  let prevYPosition = 0;
+  let direction = "up";
+
+  const options = {
+    root: scrollRoot,
+    rootMargin: `${header.offsetHeight * -1}px`,
+    threshold: 0,
+  };
+
+  const getTargetSection = (entry) => {
+    const index = sections.findIndex((section) => section == entry.target);
+
+    if (index >= sections.length - 1) {
+      return entry.target;
+    } else {
+      return sections[index + 1];
+    }
+  };
+
+  const updateColors = (target) => {
+    const theme = target.dataset.section;
+    header.setAttribute("data-theme", theme);
+    logo.setAttribute("data-theme", theme);
+  };
+
+  const shouldUpdate = (entry) => {
+    if (direction === "down" && !entry.isIntersecting) {
+      return true;
     }
 
-    // on hover view
-    const onHoverLink = document.querySelectorAll('.on-hover')
-
-    if (onHoverLink) {
-        onHoverLink.forEach((item, i) => {
-            item.addEventListener('mouseover', () => {
-                document.querySelectorAll('.on-hover-view').forEach((child) => child.classList.remove('active'))
-                document.querySelectorAll('.on-hover-view')[i].classList.add('active')
-            })
-            
-            item.addEventListener('mouseout', () => {
-                document.querySelectorAll('.on-hover-view').forEach((child) => child.classList.remove('active'))
-            })
-        })
+    if (direction === "up" && entry.isIntersecting) {
+      return true;
     }
 
-    // scroll
-    const header = document.getElementById('header')
-    const fixedLogotype = document.getElementById('fixed-logotype')
-    const imageBlocks = document.querySelectorAll('.image')
-    let prevScrollpos = window.pageYOffset
-    let coordOffsetTopHeaderCounter = 0
-    let coordOffsetTopLogotypeCounter = 0
+    return false;
+  };
 
-    if (imageBlocks) {
-        let coordOffsetTopHeaderCounter = imageBlocks.length
-        let coordOffsetTopLogotypeCounter = imageBlocks.length
+  const onIntersect = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (scrollRoot.scrollTop > prevYPosition) {
+        direction = "down";
+      } else {
+        direction = "up";
+      }
 
-        setTimeout(() => {
-            imageBlocks.forEach((item) => {
-                // header
-                if (!(window.pageYOffset >= (getCoords(item).top - header.getBoundingClientRect().height - Number(20)) && window.pageYOffset <= (getCoords(item).top + item.getBoundingClientRect().height - header.getBoundingClientRect().height - Number(20)))) {
-                    coordOffsetTopHeaderCounter--
-                }
-    
-                if (coordOffsetTopHeaderCounter > 0) {
-                    header.classList.add('header--white')
-                } else {
-                    header.classList.remove('header--white')
-                }
-    
-                // logotype
-                if (!(window.pageYOffset >= (getCoords(item).top - document.documentElement.clientHeight + Number(20)) && window.pageYOffset <= (getCoords(item).top + item.getBoundingClientRect().height - document.documentElement.clientHeight + Number(20)))) {
-                    coordOffsetTopLogotypeCounter--
-                }
-    
-                if (coordOffsetTopLogotypeCounter > 0) {
-                    fixedLogotype.classList.add('fixed-logotype--white')
-                } else {
-                    fixedLogotype.classList.remove('fixed-logotype--white')
-                }
-            })
-        }, 200)
-    }
+      prevYPosition = scrollRoot.scrollTop;
 
-    function getCoords(elem) {
-        let box = elem.getBoundingClientRect();
-        
-        return {
-            top: box.top + pageYOffset,
-            left: box.left + pageXOffset
-        };
-    }
+      const target =
+        direction === "down" ? getTargetSection(entry) : entry.target;
 
-    function headerScrollUp() {
-        if (imageBlocks) {
-            coordOffsetTopHeaderCounter = imageBlocks.length
+      if (shouldUpdate(entry)) {
+        updateColors(target);
+      }
+    });
+  };
 
-            imageBlocks.forEach(item => {
-                if (!(window.pageYOffset >= (getCoords(item).top - header.getBoundingClientRect().height) && window.pageYOffset <= (getCoords(item).top + item.getBoundingClientRect().height - header.getBoundingClientRect().height))) {
-                    coordOffsetTopHeaderCounter--
-                }
+  const observer = new IntersectionObserver(onIntersect, options);
 
-                if (coordOffsetTopHeaderCounter > 0) {
-                    header.classList.add('header--white')
-                    fixedLogotype.classList.add('fixed-logotype--white')
-                } else {
-                    header.classList.remove('header--white')
-                    fixedLogotype.classList.remove('fixed-logotype--white')
-                }
-            })
-        }
-    }
-
-    function headerScrollDown() {
-        if (imageBlocks) {
-            coordOffsetTopHeaderCounter = imageBlocks.length
-
-            imageBlocks.forEach(item => {
-                if (!(window.pageYOffset >= (getCoords(item).top - header.getBoundingClientRect().height - Number(20)) && window.pageYOffset <= (getCoords(item).top + item.getBoundingClientRect().height - header.getBoundingClientRect().height - Number(20)))) {
-                    coordOffsetTopHeaderCounter--
-                }
-
-                if (coordOffsetTopHeaderCounter > 0) {
-                    header.classList.add('header--white')
-                } else {
-                    header.classList.remove('header--white')
-                }
-            })
-        }
-    }
-
-    function fixedLogotypeScrollUp() {
-        if (imageBlocks) {
-            coordOffsetTopLogotypeCounter = imageBlocks.length
-
-            imageBlocks.forEach(item => {
-                if (!(window.pageYOffset >= (getCoords(item).top - document.documentElement.clientHeight + Number(20)) && window.pageYOffset <= (getCoords(item).top + item.getBoundingClientRect().height - document.documentElement.clientHeight + Number(20)))) {
-                    coordOffsetTopLogotypeCounter--
-                }
-
-                if (coordOffsetTopLogotypeCounter > 0) {
-                    fixedLogotype.classList.add('fixed-logotype--white')
-                } else {
-                    fixedLogotype.classList.remove('fixed-logotype--white')
-                }
-            })
-        }
-    }
-
-    function fixedLogotypeScrollDown() {
-        if (imageBlocks) {
-            coordOffsetTopLogotypeCounter = imageBlocks.length
-
-            imageBlocks.forEach(item => {
-                if (!(window.pageYOffset >= (getCoords(item).top - document.documentElement.clientHeight + Number(20)) && window.pageYOffset <= (getCoords(item).top + item.getBoundingClientRect().height - document.documentElement.clientHeight + Number(20)))) {
-                    coordOffsetTopLogotypeCounter--
-                }
-
-                if (coordOffsetTopLogotypeCounter > 0) {
-                    fixedLogotype.classList.add('fixed-logotype--white')
-                } else {
-                    fixedLogotype.classList.remove('fixed-logotype--white')
-                }
-            })
-        }
-    }
-
-    window.addEventListener('scroll', () => {
-        let currentScrollPos = window.pageYOffset;
-
-        if (prevScrollpos > currentScrollPos || prevScrollpos <= 0) { // if up
-            if (header) {
-                headerScrollUp()
-            }
-            if (fixedLogotype) {
-                fixedLogotypeScrollUp()
-            }
-        } else { // if down
-            if (header) {
-                headerScrollDown()
-            }
-            if (fixedLogotype) {
-                fixedLogotypeScrollDown()
-            }
-        }
-
-        prevScrollpos = currentScrollPos;
-    })
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
 });
